e2df2d2f18f64509cdd03dd62523c286a15ce586
Teo <m@sartori.dk>
2015-05-24 19:28:47 +0200

    Add abs, neg, divMod and cmp.

--- SwiftGMP.swift
+++ SwiftGMP.swift
@@ -78,15 +78,27 @@ extension IntBig {
 //        var newInt = IntBig()
 //        return newInt.setInt64(x)
 //    }
+
     
-//    public mutating func add(inout x: IntBig, inout y: IntBig) -> IntBig {
-public func add(x: IntBig, y: IntBig) -> IntBig {
+    public func abs(x: IntBig) -> IntBig {
+        var a = x
+        var c = self
+        __gmpz_abs(&c.i, &a.i)
+        return c
+    }
+    
+    public func neg(x: IntBig) -> IntBig {
+        var a = x
+        var c = self
+        __gmpz_neg(&c.i, &a.i)
+        return c
+    }
+
+    public func add(x: IntBig, y: IntBig) -> IntBig {
         var a = x
         var b = y
         var c = self
         __gmpz_add(&c.i, &a.i, &b.i)
-
-//        __gmpz_add(&i, &x.i, &y.i)
         return c
     }
     
@@ -115,6 +127,67 @@ public func add(x: IntBig, y: IntBig) -> IntBig {
     
     public func string() -> String {
         return inBase(10)
+    }
+    
+    // setBytes interprets buf as the bytes of a big-endian unsigned integer,
+    // sets c to that value and returns it.
+    public func setBytes(buf: [uint8]) -> IntBig {
+        var c = self
+        var b = buf
+        if buf.count == 0 {
+            c.setInt64(0)
+        } else {
+            __gmpz_import(&c.i, size_t(buf.count), 1, 1, 1, 0, &b)
+        }
+        return c
+    }
+    
+    // cmp
+    // DivMod sets z to the quotient x div y and m to the modulus x mod y
+    // and returns the pair (z, m) for y != 0.
+    // If y == 0, a division-by-zero run-time panic occurs.
+    //
+    // DivMod implements Euclidean division and modulus (unlike Go):
+    //
+    //	q = x div y  such that
+    //	m = x - y*q  with 0 <= m < |q|
+    //
+    // (See Raymond T. Boute, ``The Euclidean definition of the functions
+    // div and mod''. ACM Transactions on Programming Languages and
+    // Systems (TOPLAS), 14(2):127-144, New York, NY, USA, 4/1992.
+    // ACM press.)
+    public func divMod(x: IntBig, y: IntBig, m: IntBig) -> (IntBig, IntBig) {
+        var xl = x
+        var yl = y
+        var ml = m
+        var zl = self
+        
+        switch yl.sign() {
+        case 1:
+            __gmpz_fdiv_qr(&zl.i, &ml.i, &xl.i, &yl.i)
+        case -1:
+            __gmpz_cdiv_qr(&zl.i, &ml.i, &xl.i, &yl.i)
+        default:
+            fatalError("Division by zero")
+        }
+        return (zl, ml)
+    }
+    
+    // Cmp compares x and y and returns:
+    //
+    //   -1 if x <  y
+    //    0 if x == y
+    //   +1 if x >  y
+    public func cmp(y: IntBig) -> Int {
+        var xl = self
+        var yl = y
+        var r = Int(__gmpz_cmp(&xl.i, &yl.i))
+        if r < 0 {
+            r = -1
+        } else if r > 0 {
+            r = 1
+        }
+        return r
     }
     
     public func bytes() -> [uint8] {
